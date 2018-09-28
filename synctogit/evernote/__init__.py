e36dec7a5f6c664ce7a71bf77a53c511f5305dda
Kostya Esmukov <kostya@esmukov.ru>
2018-09-29 00:04:23 +0300

    Refactor the God Sync loop

--- synctogit/evernote/__init__.py
+++ synctogit/evernote/__init__.py
@@ -11,7 +11,8 @@ from .. import index_generator
 from ..git_transaction import GitTransaction
 from .auth import InteractiveAuth, UserCancelledError
 from .evernote import Evernote
-from .working_copy import EvernoteWorkingCopy
+from .models import NoteGuid, NoteMetadata
+from .working_copy import Changeset, EvernoteWorkingCopy
 
 logger = logging.getLogger(__name__)
 
@@ -80,141 +81,182 @@ class EvernoteSync(BaseSync[EvernoteAuthSession]):
             sandbox=self.config.get_bool('evernote', 'sandbox', False),
         )
         evernote.auth(self.auth_session.token)
-        self._god_sync(evernote)
+        self._sync_loop(evernote)
 
-    def _god_sync(self, evernote):
+    def _sync_loop(self, evernote):
         git_conf = {
             'push': self.config.get_bool('git', 'push', False),
             'remote_name': self.config.get_str('git', 'remote_name', 'origin'),
         }
 
         any_fail = False
-        updates = [True]
-        while updates[0]:
-            updates[0] = False
+        is_converged = False
+        while not is_converged:
+            is_converged = True
+            any_fail = False
+
+            logger.info("Starting sync iteration...")
 
             with GitTransaction(self.git,
                                 remote_name=git_conf["remote_name"],
                                 push=git_conf["push"]) as t:
                 wc = EvernoteWorkingCopy(t)
+
+                logger.info("Retrieving actual metadata...")
+                working_copy_metadata, evernote_metadata = (
+                    self._retrieve_metadata(wc=wc, evernote=evernote)
+                )
+
                 logger.info("Calculating changes...")
-
-                # Gather metadata from both Evernote and the git repo simultaneously.
-                with ThreadPoolExecutor(max_workers=2) as pool:
-                    working_copy_metadata, evernote_metadata = pool.map(
-                        lambda f: f(),
-                        (
-                            wc.get_working_copy_metadata,
-                            evernote.get_actual_metadata,
-                        )
-                    )
-
-                update = wc.calculate_changes(
+                changeset = wc.calculate_changes(
                     working_copy_metadata=working_copy_metadata,
                     evernote_metadata=evernote_metadata,
                     force_update=self.force_full_resync,
                 )
+
                 logger.info("Applying changes...")
+                wc.delete_notes(changeset.delete)
+                update_context = self._update_notes(changeset, wc, evernote)
 
-                wc.delete_notes(update.delete)
+                is_converged = is_converged and update_context.is_converged
 
-                queue = []
-                for op in ['new', 'update']:
-                    for guid in getattr(update, op):
-                        queue.append([op, guid, getattr(update, op)[guid]])
+                logger.info("Updating index...")
+                self._update_index(evernote_metadata)
 
-                total = len(queue)
-                saved = [0]
-                failed = [0]
-
-                lock = threading.Lock()
-
-                def job():
-                    while True:
-                        lock.acquire()
-                        try:
-                            j = queue.pop()
-                            i = total - len(queue)
-                        except Exception:
-                            return
-                        finally:
-                            lock.release()
-
-                        guid = j[1]
-                        d = j[2]
-                        logger.info(
-                            "Getting note (%d/%d) contents: %s...", i, total, guid
-                        )
-
-                        try:
-                            note = evernote.get_note(
-                                guid, wc.get_relative_resources_url(guid, d)
-                            )
-                        except Exception as e:
-                            logger.warning(
-                                "Unable to get the note %s: %s", guid, repr(e)
-                            )
-                            lock.acquire()
-                            failed[0] += 1
-                            lock.release()
-                            continue
-
-                        lock.acquire()
-                        try:
-                            saved[0] += 1
-                            if note.update_sequence_num == d.update_sequence_num:
-                                logger.info(
-                                    "Saving note (%d/%d) contents: %s...",
-                                    saved[0],
-                                    total,
-                                    guid,
-                                )
-                                wc.save_note(note, d)
-                            else:
-                                logger.info(
-                                    "Skipping note (%d/%d) because it has changed "
-                                    "during sync: %s...",
-                                    saved[0],
-                                    total,
-                                    guid,
-                                )
-                                updates[0] = True
-                        finally:
-                            lock.release()
-
-                jobs = []
-                for j in range(
-                    self.config.get_int("internals", "notes_download_threads", 30)
-                ):
-                    jobs.append(threading.Thread(target=job))
-
-                for j in jobs:
-                    j.start()
-
-                for j in jobs:
-                    j.join()
-
-                note_links = [
-                    (note.dir + (note.file,), note.name)
-                    for note in evernote_metadata.values()
-                ]
-                index_generator.generate(
-                    note_links,
-                    index_generator.file_writer(
-                        os.path.join(self.config.get_str('git', 'repo_dir'), "index.html")
-                    ),
-                )
-                logger.info("Sync loop ended.")
+                logger.info("Sync iteration is done!")
                 logger.info(
                     "Target was: delete: %d, create: %d, update: %d",
-                    len(update.delete),
-                    len(update.new),
-                    len(update.update),
+                    len(changeset.delete),
+                    len(changeset.new),
+                    len(changeset.update),
                 )
-                logger.info("Result: saved: %d, failed: %d", saved[0], failed[0])
-                any_fail = failed[0] != 0
+                logger.info(
+                    "Result: saved: %d, failed: %d",
+                    len(update_context.updated_notes),
+                    len(update_context.failed_notes),
+                )
+                any_fail = len(update_context.failed_notes) != 0
 
         logger.info("Done")
 
         if any_fail:
             raise Exception("Sync done with fails")
+
+    def _retrieve_metadata(self, *, wc: EvernoteWorkingCopy, evernote: Evernote):
+        # Gather metadata from both Evernote and the git repo simultaneously.
+        with ThreadPoolExecutor(max_workers=2) as pool:
+            a = pool.submit(wc.get_working_copy_metadata)
+            b = pool.submit(evernote.get_actual_metadata)
+
+            working_copy_metadata = a.result()
+            evernote_metadata = b.result()
+            return working_copy_metadata, evernote_metadata
+
+    def _update_notes(
+        self,
+        changeset: Changeset,
+        wc: EvernoteWorkingCopy,
+        evernote: Evernote,
+    ) -> '_UpdateContext':
+        notes_to_update = [
+            (guid, note_metadata)
+            for op in ['new', 'update']
+            for guid, note_metadata in getattr(changeset, op).items()
+        ]
+        update_context = _UpdateContext(total=len(notes_to_update))
+
+        max_workers = self.config.get_int(
+            "internals", "notes_download_threads", 30
+        )
+
+        with ThreadPoolExecutor(max_workers=max_workers) as pool:
+            for guid, note_metadata in notes_to_update:
+                pool.submit(self._update_note, guid, note_metadata,
+                            update_context, wc, evernote)
+        return update_context
+
+    def _update_note(
+        self,
+        note_guid: NoteGuid,
+        note_metadata: NoteMetadata,
+        update_context: '_UpdateContext',
+        wc: EvernoteWorkingCopy,
+        evernote: Evernote,
+    ) -> None:
+        current = update_context.start()
+
+        logger.info(
+            "Getting note (%d/%d) contents: %s...",
+            current, update_context.total, note_guid
+        )
+
+        try:
+            note = evernote.get_note(
+                note_guid,
+                wc.get_relative_resources_url(note_guid, note_metadata)
+            )
+        except Exception as e:
+            logger.warning(
+                "Unable to get the note %s: %s", note_guid, repr(e)
+            )
+            update_context.add_failed(note_guid, note_metadata)
+            return
+
+        if note.update_sequence_num == note_metadata.update_sequence_num:
+            logger.info(
+                "Saving note (%d/%d) contents: %s...",
+                current,
+                update_context.total,
+                note_guid,
+            )
+            wc.save_note(note, note_metadata)
+            update_context.add_updated(note_guid, note_metadata)
+        else:
+            logger.info(
+                "Skipping note (%d/%d) because it has changed "
+                "during sync: %s...",
+                current,
+                update_context.total,
+                note_guid,
+            )
+            update_context.set_not_converged()
+
+    def _update_index(self, evernote_metadata) -> None:
+        note_links = [
+            (note.dir + (note.file,), note.name)
+            for note in evernote_metadata.values()
+        ]
+        index_generator.generate(
+            note_links,
+            index_generator.file_writer(
+                os.path.join(self.config.get_str('git', 'repo_dir'), "index.html")
+            ),
+        )
+
+
+class _UpdateContext:
+    def __init__(self, total: int):
+        self.started = 0
+        self.total = total
+        self.updated_notes = []  # type: List[Tuple[NoteGuid, NoteMetadata]]
+        self.failed_notes = []  # type: List[Tuple[NoteGuid, NoteMetadata]]
+        self.is_converged = True
+        self.lock = threading.Lock()
+
+    def start(self):
+        with self.lock:
+            self.started += 1
+            current = self.started
+        return current
+
+    def set_not_converged(self):
+        with self.lock:
+            self.is_converged = False
+
+    def add_updated(self, note_guid, note_metadata):
+        with self.lock:
+            self.updated_notes.append((note_guid, note_metadata))
+
+    def add_failed(self, note_guid, note_metadata):
+        with self.lock:
+            self.failed_notes.append((note_guid, note_metadata))
