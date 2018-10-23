2c57655ba7c1634ee254168a68f8f361a245a1ea
Kostya Esmukov <kostya@esmukov.ru>
2018-10-23 22:01:27 +0300

    OneNote: add WorkingCopy

---
+++ synctogit/onenote/working_copy.py
@@ -0,0 +1,60 @@
+from pathlib import Path
+from typing import Sequence, Tuple
+
+import pytz
+
+from synctogit.service.notes import Changeset, NoteResource, WorkingCopy
+
+from .models import OneNotePage, OneNotePageId, OneNotePageMetadata
+from .stored_note import OneNoteStoredNote
+
+
+class OneNoteChangeset(Changeset[OneNotePageId, OneNotePageMetadata]):
+    pass
+
+
+class OneNoteWorkingCopy(
+    WorkingCopy[OneNotePageId, OneNotePageMetadata, OneNoteChangeset]
+):
+    # This class must be thread-safe
+
+    changeset_cls = OneNoteChangeset  # type: TChangeSet
+
+    @classmethod
+    def _metadata_dir(cls, metadata: OneNotePageMetadata) -> Sequence[str]:
+        return metadata.dir
+
+    @classmethod
+    def _metadata_file(cls, metadata: OneNotePageMetadata) -> str:
+        return metadata.file
+
+    @classmethod
+    def _is_moved_note(cls, m1: OneNotePageMetadata, m2: OneNotePageMetadata) -> bool:
+        return m1.name != m2.name
+
+    @classmethod
+    def _is_updated_note(cls, m1: OneNotePageMetadata, m2: OneNotePageMetadata) -> bool:
+        dt1 = m1.last_modified.astimezone(pytz.utc)
+        dt2 = m2.last_modified.astimezone(pytz.utc)
+        return dt1 != dt2
+
+    def _get_stored_note_metadata(
+        self, notes_dir, note_path: Path
+    ) -> Tuple[OneNotePageId, OneNotePageMetadata]:
+        return OneNoteStoredNote.get_stored_note_metadata(
+            notes_dir, note_path
+        )
+
+    def save_note(self, note: OneNotePage, metadata: OneNotePageMetadata):
+        super()._save_note(
+            note_key=note.info.id,
+            metadata=metadata,
+            html_body=OneNoteStoredNote.note_to_html(note, self.timezone),
+            resources=[
+                NoteResource(
+                    filename=r.filename,
+                    body=r.body,
+                )
+                for r in note.resources.values()
+            ]
+        )
---
+++ tests/onenote/test_working_copy.py
@@ -0,0 +1,61 @@
+import datetime
+
+import pytest
+import pytz
+
+from synctogit.onenote.models import OneNotePageMetadata
+from synctogit.onenote.working_copy import OneNoteWorkingCopy
+
+tz1 = pytz.timezone("Europe/Moscow")
+tz2 = pytz.timezone("Asia/Novosibirsk")
+
+
+@pytest.mark.parametrize(
+    'd1, d2, is_updated',
+    [
+        (
+            # Equal -- not updated
+            tz1.localize(datetime.datetime(2018, 10, 1, 10, 10, 10)),
+            tz1.localize(datetime.datetime(2018, 10, 1, 10, 10, 10)),
+            False,
+        ),
+        (
+            # One is greater -- updated
+            tz1.localize(datetime.datetime(2018, 10, 1, 10, 10, 10)),
+            tz1.localize(datetime.datetime(2018, 10, 2, 10, 10, 10)),
+            True,
+        ),
+        (
+            # Another is greater -- updated
+            tz1.localize(datetime.datetime(2018, 10, 2, 10, 10, 10)),
+            tz1.localize(datetime.datetime(2018, 10, 1, 10, 10, 10)),
+            True,
+        ),
+        (
+            # Same local dates in different timezones -- updated
+            tz1.localize(datetime.datetime(2018, 10, 1, 10, 10, 10)),
+            tz2.localize(datetime.datetime(2018, 10, 1, 10, 10, 10)),
+            True,
+        ),
+        (
+            # Same points in time in different timezones -- not updated
+            tz1.localize(datetime.datetime(2018, 10, 1, 10, 10, 10)),
+            tz2.localize(datetime.datetime(2018, 10, 1, 14, 10, 10)),
+            False,
+        ),
+    ]
+)
+def test_is_updated_note(d1, d2, is_updated):
+    m1 = OneNotePageMetadata(
+        dir=('a', 'b'),
+        file='c',
+        name=('a', 'b', 'c'),
+        last_modified=d1,
+    )
+    m2 = OneNotePageMetadata(
+        dir=('a', 'b'),
+        file='c',
+        name=('a', 'b', 'c'),
+        last_modified=d2,
+    )
+    assert is_updated is OneNoteWorkingCopy._is_updated_note(m1, m2)
